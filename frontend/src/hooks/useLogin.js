a8cb99885f1d54d931a1521206126f85c8fdd57c
ankit-mr-me <90814639+ankit-mr-me@users.noreply.github.com>
2025-02-09 03:20:08 +0530

    Update useLogin.js

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -6,6 +6,7 @@ export const useLogin = () => {
     const [isLoading, setIsLoading] = useState(null)
     const { dispatch } = useAuthContext()
 
+    
     const login = async (email, password) => {
     setIsLoading(true)
     setError(null)
