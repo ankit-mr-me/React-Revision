59aab80255e7cee86a80c176888dafad26028de4
ankit-mr-me <90814639+ankit-mr-me@users.noreply.github.com>
2025-02-09 03:28:23 +0530

    copied useLogin.js from local server

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -6,7 +6,6 @@ export const useLogin = () => {
     const [isLoading, setIsLoading] = useState(null)
     const { dispatch } = useAuthContext()
 
-    
     const login = async (email, password) => {
     setIsLoading(true)
     setError(null)
