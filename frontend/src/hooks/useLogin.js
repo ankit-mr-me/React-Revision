17bfb760b38b1884efe9213187d8256d4bb2dcbb
ankit-mr-me <90814639+ankit-mr-me@users.noreply.github.com>
2025-02-09 14:23:46 +0530

    patch 2

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -12,7 +12,7 @@ export const useLogin = () => {
 
     const response = await fetch('/api/user/login', {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: {'Content-Type': 'application/json; charset=utf-8'},
         body: JSON.stringify({ email, password })
     })
     const json = await response.json()
